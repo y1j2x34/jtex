d8c99dafa1e7bc3f128bed7bddf2ae3ef1a1b573
y1j2x34 <y1j2x34@qq.com>
2020-10-17 11:11:57 +0800

    feat: getSystemInfo API

---
+++ packages/jtex-chrome-extension/src/background/handlers/getSystemInfo.ts
@@ -0,0 +1,38 @@
+import Defer from '../../common/defer';
+import { ActionOption } from './types';
+
+type FunctionPassingCallback = (callback: (data: any) => void) => void;
+
+type CallbackParameter<T extends FunctionPassingCallback> = T extends (callback: (data: infer P) => void) => void
+    ? P
+    : never;
+
+function promisify<T extends FunctionPassingCallback, R = CallbackParameter<T>>(
+    method: T,
+    context: any
+): () => Promise<R> {
+    return () => {
+        const defer = new Defer<R>();
+        method.call(context, (data: R) => {
+            defer.resolve(data);
+        });
+        return defer.promise;
+    };
+}
+
+export function getSystemInfo(_: ActionOption) {
+    const memory = chrome.system.memory;
+    const storage = chrome.system.storage;
+    const cpu = chrome.system.cpu;
+    return Promise.all([
+        promisify<typeof cpu.getInfo>(cpu.getInfo, cpu)(),
+        promisify<typeof memory.getInfo>(memory.getInfo, memory)(),
+        promisify<typeof storage.getInfo>(storage.getInfo, storage)()
+    ]).then(([cpuInfo, memoryInfo, storageUnitInfoArray]) => {
+        return {
+            cpu: cpuInfo,
+            memory: memoryInfo,
+            storage: storageUnitInfoArray
+        };
+    });
+}
--- packages/jtex-chrome-extension/src/background/handlers/index.ts
+++ packages/jtex-chrome-extension/src/background/handlers/index.ts
@@ -1,3 +1,4 @@
 export { captureAndSave } from './captureAndSave';
 export { download } from './download';
 export { capture } from './capture';
+export { getSystemInfo } from './getSystemInfo';
