fdf1c6d77067fc87bcf8fc0a65a0537f7ffbcd16
y1j2x34 <y1j2x34@qq.com>
2021-01-31 23:21:27 +0800

    feat: json report data types

---
+++ common/jtex-karma-json-report-core/index.ts
@@ -0,0 +1,55 @@
+export interface AssertionError {
+    name: string;
+    message: string;
+    showDiff: true,
+    actual: string;
+    expected: string;
+}
+export interface SuiteResultInfo {
+    description: string;
+    suite: string[];
+    success: boolean;
+    skipped: boolean;
+    pending: boolean;
+    time: number;
+    log?: string[];
+    assertionErrors: AssertionError[];
+    startTime: number;
+    endTime: number;
+}
+
+export interface BrowserLastResult {
+    startTime: number;
+    total: number;
+    success: number;
+    failed: number;
+    skipped: number;
+    totalTime: number;
+    netTime: number;
+    error: false;
+}
+
+export interface BrowserInfo {
+    id: string;
+    fullName: string;
+    name: string;
+    lastResult: BrowserLastResult;
+}
+export interface TestSummary {
+    success: number;
+    failed: number;
+    skipped: number;
+    error: boolean;
+    disconnected: boolean;
+    exitCode: number;
+}
+export interface BrowserStats {
+    browser: BrowserInfo;
+    errors: Array<any>;
+    results: Array<SuiteResultInfo>;
+}
+
+export interface JSONReportData {
+    summary: TestSummary;
+    browsers: BrowserStats[];
+}
