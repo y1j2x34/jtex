0d0724f40d6f8c96615a99c46a6ee32760d41c3b
y1j2x34 <y1j2x34@qq.com>
2021-04-15 23:31:20 +0800

    chore: remove useless package

--- packages/jtex-firefox-extension/build/webpack/webpack.build.ts
+++
@@ -1,8 +0,0 @@
-import baseConfig from './webpack.dev';
-import webpackMerge from 'webpack-merge';
-
-export default webpackMerge(baseConfig, {
-    watch: false,
-    mode: 'production',
-    devtool: 'nosources-source-map'
-});
